534b5fa2e92b3eb756fb91abe725217e27ab1149
pg-goose <pau.galopa.dev@gmail.com>
2024-12-14 18:52:17 +0100

    models done

--- OutbreakDetector/myapp/models.py
+++ OutbreakDetector/myapp/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
@@ -24,13 +25,22 @@ class Informe(models.Model):
         MAL_DE_PANXA = "MAL_DE_PANXA", _("Mal De Panxa")
         CALFREDS = "CALFREDS", _("Calfreds")
         MAL_DE_CAP = "MAL_DE_CAP", _("Mal De Cap")
-        MAL_DE_COLL = "MAL_DE_COLL", _("Mal De_COLL")
-        MOCS = "MOCS", _("MOCS")
-        NAS_TAPAT = "NAS_TAPAT", _("NAS_TAPAT")
-        ESTERNUT = "ESTERNUT", _("ESTERNUT")
-        VOMITS = "VOMITS", _("VOMITS")
-        TOS = "TOS", _("TOS")
-        ALTRES = "ALTRES", _("ALTRES")
+        MAL_DE_COLL = "MAL_DE_COLL", _("Mal De Coll")
+        MOCS = "MOCS", _("Mocs")
+        NAS_TAPAT = "NAS_TAPAT", _("Nas Tapat")
+        ESTERNUT = "ESTERNUT", _("Esternut")
+        VOMITS = "VOMITS", _("Vomits")
+        TOS = "TOS", _("Tos")
+        ALTRES = "ALTRES", _("Altres")
+
+    data = models.DateField()
+    curs = models.ForeignKey(Curs, on_delete=models.CASCADE)
+    simptoma = models.CharField(
+        max_length=50,
+        choices=Simptoma.choices,
+        default=Simptoma.ALTRES,
+    )
+
         
         
         
