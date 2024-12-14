85e266c8b47cdf9844734c1942c71c59a22641ed
pg-goose <pau.galopa.dev@gmail.com>
2024-12-14 19:23:51 +0100

    .

--- OutbreakDetector/myapp/models.py
+++ OutbreakDetector/myapp/models.py
@@ -1,5 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Escola(models.Model):
--- OutbreakDetector/myapp/views.py
+++ OutbreakDetector/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+class RedirectUserView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        user = self.request.user
+        if user.is_superuser:
+            return '/admin/'
+        else:
+            return '/alertas/'
+
+class AdminOutbreaksView(LoginRequiredMixin, TemplateView):
+    template_name = 'admin_outbreaks.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['outbreaks'] = Outbreak.objects.all()
+        return context
